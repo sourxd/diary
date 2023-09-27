ba69af38922cc0253785f622f18892ff04a7a0aa
Oleg Barovskiy <oleg.sour@gmail.com>
2023-09-27 17:53:49 +0300

    new items (заметки)

--- users/models.py
+++ users/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Notes(models.Model):
+    name = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
